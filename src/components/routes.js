1d91031477ff4bd3532f555b4d5a637b3cae9cb0
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-29 14:04:15 -0500

    Added history package and component, WIP: Routes and Pricing page

---
+++ src/components/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import 
+
+const Routes = () => {
+    return(
+        <Switch>
+            <Route path="/" exact component={}/>
+            <Route path="/plans" exact component={}/>
+        </Switch>
+    )
+}
