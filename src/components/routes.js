0a7f8ce9f65bf06a4a08c8b64f416e63c2487023
Hasan Jafri <hasan.jafri7@gmail.com>
2018-12-29 14:09:57 -0500

    Update App.js for sample route /plans linking to pricing page

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import 
+import Pricing from './pages/pricing';
 
 const Routes = () => {
     return(
         <Switch>
-            <Route path="/" exact component={}/>
-            <Route path="/plans" exact component={}/>
+            <Route path="/" exact component={Pricing}/>
+            <Route path="/plans" exact component={Pricing}/>
         </Switch>
     )
 }
+
+export default Routes;
