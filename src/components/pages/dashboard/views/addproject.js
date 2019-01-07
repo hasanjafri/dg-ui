bd47ca37091607e712f1a5bbaf8cbce2deee6486
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-07 16:59:49 -0500

    Add form to add project page

--- src/components/pages/dashboard/views/addproject.js
+++ src/components/pages/dashboard/views/addproject.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Footer from '../footer';
 import SideNavBar from '../sidenavbar';
+import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import Paper from '@material-ui/core/Paper';
-import Select from '@material-ui/core/Select';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -30,16 +29,21 @@ const styles = theme => ({
         flexDirection: 'column',
         alignItems: 'center',
         padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+        maxWidth: '400px'
     },
     form: {
         width: '100%', // Fix IE 11 issue.
         marginTop: theme.spacing.unit,
     },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
 })
 
 class AddProject extends React.Component {
     state = {
-        email: ''
+        projectName: '',
+        projectAddress: ''
     }
 
     handleChange = name => event => {
@@ -63,16 +67,16 @@ class AddProject extends React.Component {
                             </Typography>
                             <form className={classes.form} onSubmit={this.handleSubmit}>
                                 <FormControl margin="normal" required fullWidth>
-                                    <InputLabel htmlFor="email">Email Address</InputLabel>
-                                    <Input value={this.state.email} onChange={this.handleChange('email')} id="email" name="email" autoFocus disableUnderline/>
+                                    <InputLabel htmlFor="projectName">Project Name</InputLabel>
+                                    <Input value={this.state.projectName} onChange={this.handleChange('projectName')} name="projectName" id="projectName" disableUnderline/>
                                 </FormControl>
                                 <FormControl margin="normal" required fullWidth>
-                                    <InputLabel htmlFor="password">Password</InputLabel>
-                                    <Input value={this.state.password} onChange={this.handleChange('password')} name="password" type="password" id="password" disableUnderline/>
+                                    <InputLabel htmlFor="address">Address</InputLabel>
+                                    <Input value={this.state.projectAddress} onChange={this.handleChange('projectAddress')} name="projectAddress" id="projectAddress" disableUnderline/>
                                 </FormControl>
-                                <FormControl >
-                                
-                                </FormControl>
+                                <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                                    Add project
+                                </Button>
                             </form>
                         </Paper>
                     </main>
--- src/components/pages/dashboard/views/adduser.js
+++ src/components/pages/dashboard/views/adduser.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Footer from '../footer';
 import SideNavBar from '../sidenavbar';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -18,9 +23,36 @@ const styles = theme => ({
     tableContainer: {
       height: 320,
     },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+    },
 })
 
 class AddUser extends React.Component {
+    state = {
+        email: '',
+        emailError: false,
+        password: '',
+        passwordError: false,
+        firstName: '',
+        lastName: '',
+
+    }
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value
+        });
+    }
+
     render() {
         const { classes } = this.props;
 
@@ -30,9 +62,24 @@ class AddUser extends React.Component {
                     <SideNavBar/>
                     <main className={classes.content}>
                         <div className={classes.appBarSpacer} />
-                        <Typography variant="h4" gutterBottom component="h2">
-                            Add a User to your project
-                        </Typography>
+                        <Paper className={classes.paper}>
+                            <Typography component="h1" variant="h6">
+                                Add User
+                            </Typography>
+                            <form className={classes.form} onSubmit={this.handleSubmit}>
+                                <FormControl margin="normal" required fullWidth>
+                                    <InputLabel htmlFor="email">Email Address</InputLabel>
+                                    <Input value={this.state.email} onChange={this.handleChange('email')} id="email" name="email" autoFocus disableUnderline/>
+                                </FormControl>
+                                <FormControl margin="normal" required fullWidth>
+                                    <InputLabel htmlFor="password">Password</InputLabel>
+                                    <Input value={this.state.password} onChange={this.handleChange('password')} name="password" type="password" id="password" disableUnderline/>
+                                </FormControl>
+                                <FormControl>
+
+                                </FormControl>
+                            </form>
+                        </Paper>
                     </main>
                 </div>
                 <Footer/>
