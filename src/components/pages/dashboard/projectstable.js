120a70d8246ac049368d6e19927738ff29498438
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-07 20:24:00 -0500

    WIP: add project page on dashboard

---
+++ src/components/pages/dashboard/projectstable.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = {
+    root: {
+        width: '100%',
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700
+    },
+};
+
+function ProjectsTable(props) {
+    const { classes, projectsData } = props;
+
+    return(
+        <Paper className={classes.root}>
+            <Table className={classes.table}>
+                <TableHead>
+                <TableRow>
+                    <TableCell>Project Name</TableCell>
+                    <TableCell>Address</TableCell>
+                    <TableCell align="right"># of Users</TableCell>
+                    <TableCell align="right">Last Updated</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {projectsData !== undefined && projectsData.map((n, i) => {
+                    return (
+                    <TableRow key={i}>
+                        <TableCell component="th" scope="row">
+                        {n.project_name}
+                        </TableCell>
+                        <TableCell>{n.address}</TableCell>
+                        <TableCell align="right">{n.num_users}</TableCell>
+                        <TableCell align="right">{n.last_updated}</TableCell>
+                    </TableRow>
+                    );
+                })}
+                </TableBody>
+            </Table>
+        </Paper>
+    )    
+}
+
+ProjectsTable.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ProjectsTable);
