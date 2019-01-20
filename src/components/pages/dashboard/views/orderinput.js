e3f52e4d65344a09c4b1e2f3838dc1e4b803e395
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-19 20:35:53 -0500

    WIP: OrderInput

--- src/components/pages/dashboard/views/orderinput.js
+++ src/components/pages/dashboard/views/orderinput.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import history from 'history';
+import Paper from '@material-ui/core/Paper';
+import SideNavBar from '../sidenavbar';
+import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
-
+    root: {
+        display: 'flex',
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+        height: '100vh',
+        overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+        maxWidth: '400px',
+        height: '100%'
+    },
+    projectTitle: {
+        marginBottom: theme.spacing.unit,
+    },
 });
 
 class OrderInput extends React.Component {
     state = {
-        
+        projects: null
     }
 
-    loadAdminProjects = () => {
+    loadProjects = () => {
         fetch('http://192.168.99.100:6969/api/project', {
             mode: 'cors',
             credentials: 'include'
@@ -26,6 +53,32 @@ class OrderInput extends React.Component {
                 console.log(responseJson.projects);
             }
         }).catch(error => console.error('Error:', error));
+    }
+
+    componentDidMount() {
+        this.loadProjects();
+    }
+
+    render() {
+        const { classes } = this.props;
+
+        return(
+            <React.Fragment>
+                <div className={classes.root}>
+                    <SideNavBar/>
+                    <main className={classes.content}>
+                        <div className={classes.appBarSpacer}>
+                            <Paper className={classes.paper}>
+                                <Typography component="h1" variant="h6" className={classes.projectTitle}>
+                                    Select Project
+                                </Typography>
+                                <Select autoFocus fullWidth value=
+                            </Paper>
+                        </div>
+                    </main>
+                </div>
+            </React.Fragment>
+        );
     }
 }
 
