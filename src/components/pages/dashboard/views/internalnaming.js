2f826389e8b7dddcfa830f34695833fb34990816
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-23 01:48:42 -0500

    WIP: internal naming

--- src/components/pages/dashboard/views/internalnaming.js
+++ src/components/pages/dashboard/views/internalnaming.js
@@ -54,7 +54,13 @@ class InternalNaming extends React.Component {
     state = {
         projects: null,
         projectId: '',
-        category_name: ''
+        category_name: '',
+        categoryError: false,
+        projectCategories: null,
+        categoryId: '',
+        response: '',
+        internal_name: '',
+        internalNameError: false
     };
 
     loadProjects = () => {
@@ -64,7 +70,7 @@ class InternalNaming extends React.Component {
         })
         .then(res => res.json()).then(responseJson => {
             if (responseJson.error) {
-                history.push('/login')
+                history.push('/login');
             } else if (responseJson.projects) {
                 this.setState({
                     projects: responseJson.projects
@@ -75,17 +81,107 @@ class InternalNaming extends React.Component {
     }
 
     loadCategories = () => {
-        
+        fetch('http://192.168.99.100:6969/api/category', {
+            mode: 'cors',
+            credentials: 'include'
+        })
+        .then(res => res.json()).then(json => {
+            if (json.error) {
+                history.push('/login');
+            } else if (json.categories) {
+                this.setState({
+                    projectCategories: json.categories
+                });
+                console.log(json.categories);
+            } else {
+                console.log(json);
+            }
+        }).catch(err => console.error('Error: ', err));
     }
 
     componentDidMount() {
         this.loadProjects();
+        this.loadCategories();
     }
 
     handleChange = name => event => {
         this.setState({
             [name]: event.target.value
         })
+    }
+
+    generateBodyDict = (type) => {
+        if (type === 'category') {
+            let categoryErrorCheck = this.state.category_name === "";
+
+            if (categoryErrorCheck) {
+                this.setState({
+                    categoryError: categoryErrorCheck
+                })
+                return {}
+            } else {
+                this.setState({
+                    categoryError: false
+                });
+                return {
+                    "project_id": this.state.projectId,
+                    "category_name": this.state.category_name
+                }
+            }
+        } else if (type === 'internal_name') {
+
+        }
+    }
+
+    handleSubmit = type => event => {
+        event.preventDefault();
+        let resBody = this.generateBodyDict(type);
+
+        if (resBody === {}) {
+            return;
+        } else {
+            if (type === 'category') {
+                fetch('http://192.168.99.100:6969/api/category', {
+                    mode: 'cors',
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(resBody)
+                }).then(res => res.json()).then(json => {
+                    if (json.error) {
+                        history.push('/login');
+                    } else if (json.msg) {
+                        this.setState({
+                            response: json.msg
+                        });
+                    } else {
+                        console.log(json);
+                    }
+                }).catch(err => console.error('Error: ', err));
+            } else if (type === 'internal_name') {
+                fetch('http://192.168.99.100:6969/api/internal_name', {
+                    mode: 'cors',
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(resBody)
+                }).then(res => res.json()).then(json => {
+                    if (json.error) {
+                        history.push('/login');
+                    } else if (json.msg) {
+                        this.setState({
+                            response: json.msg
+                        });
+                    } else {
+                        console.log(json);
+                    }
+                }).catch(err => console.error('Error: ', err));
+            }
+        }
     }
 
     render() {
